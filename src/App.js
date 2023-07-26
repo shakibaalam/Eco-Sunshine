c39612a50c8f756fab053b75e8240fc06d80dc20
MD MONIRUZZAMAN <96796294+Moniruzzaman2525@users.noreply.github.com>
2023-07-26 08:59:15 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, RouterProvider } from "react-router-dom";
 import "./App.css";
 import router from "./layout/router";
 import { store } from "./redux/store/store";
